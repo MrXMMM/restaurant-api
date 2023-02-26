a74c643cc67f929aae1f78fdde223ec5bfca10ee
Phutawan Sarasuk <phutawan1231@gmail.com>
2023-02-26 15:30:41 +0700

    Ready to deploy backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -119,8 +119,9 @@ const login = asyncHandler(async (req, res) => {
      res.cookie('jwt', refreshToken, {
         httpOnly: true, //accessible only by web server 
         secure: true, //https
-        //sameSite: 'None', //cross-site cookie 
-        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
+        sameSite: 'None', //cross-site cookie 
+        maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+        Secure
     })
     // Send accessToken containing email and employee info
     res.json({ accessToken })
